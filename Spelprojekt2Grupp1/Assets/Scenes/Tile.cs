04e295792e5a6a409f0339544270ea02cf29f72a
jesper-lundgren <70626985+jesper-lundgren@users.noreply.github.com>
2020-11-13 18:08:55 +0100

    Created Tile base class
    A base class for tiles with special behaviour such as freezers, ovens, fans etc

---
+++ Spelprojekt2Grupp1/Assets/Scenes/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for all tiles in Unity scenes
+/// </summary>
+public class Tile : MonoBehaviour
+{
+    /// <summary>
+    /// Executes a behaviour according its type
+    /// </summary>
+    /// <param name="aGameActor">The gameobject belonging to the Player</param>
+    public void TGAExecute(GameObject aGameActor) { }
+}
