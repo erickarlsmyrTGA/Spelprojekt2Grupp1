4fee80b605b0657ab42abbd32390448034e9f044
jesper-lundgren <70626985+jesper-lundgren@users.noreply.github.com>
2020-11-14 17:42:01 +0100

    Created Player script with basic game logic

---
+++ Spelprojekt2Grupp1/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool myCoroutineIsNotInAction;
+
+    private void Start()
+    {
+        myCoroutineIsNotInAction = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Always tries to run GameLogic
+        if (myCoroutineIsNotInAction)
+        {
+            StartCoroutine(GameLogicCoroutine());
+        }
+    }
+
+    IEnumerator GameLogicCoroutine()
+    {
+        // Mark Coroutine has started
+        myCoroutineIsNotInAction = false;
+
+        yield return StartCoroutine(HandleMouseInput());
+        yield return StartCoroutine(ExecuteCurrentTile());
+        yield return StartCoroutine(CheckFallDistanceAndFall());
+
+        // Mark Coroutine has ended
+        myCoroutineIsNotInAction = true;
+    }
+
+    IEnumerator HandleMouseInput()
+    {
+        // [2020-11-14 17:37][Jesper]
+        // Example code 
+        // Wait for Mouse Input
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+
+        // TODO: Implement this
+    }
+
+    IEnumerator ExecuteCurrentTile()
+    {
+        var tile = TileManager.ourInstance.TGATryGetTileAt(transform.position);
+        if (tile)
+        {
+            yield return StartCoroutine(tile.TGAExecute(gameObject));
+        }
+    }
+
+    IEnumerator CheckFallDistanceAndFall()
+    {
+        // TODO: Implement this
+        yield return null;
+    }
+}
