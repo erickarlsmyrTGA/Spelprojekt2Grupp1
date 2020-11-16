3965435242b65839b025e0971b893f132141d78e
PerryIvic <71003066+PerryIvic@users.noreply.github.com>
2020-11-16 18:25:10 +0100

    Tile_Button (Follows corutine)

--- Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tile_Button.cs
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tile_Button.cs
@@ -8,7 +8,7 @@ public class Tile_Button : Tile
     [SerializeField]
     Tile myTargetTile;
 
-    private bool myRePressableButton = false;
+    private bool myButtonIsActive = false;
 
     Tile_Button()
     {
@@ -22,8 +22,29 @@ public class Tile_Button : Tile
     /// <returns></returns>
     public virtual IEnumerator TGAExecute(GameObject aGameActor)
     {
+        yield return ToggleButton();
+    }
+
+    /// <summary>
+    /// Toggles animation for tile on animation-event
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ToggleButton()
+    {
         // Get tile or a function from tile
-        Debug.Log("Button is pressed");
-        yield return null;
+        if (myButtonIsActive == false)
+        {
+            myButtonIsActive = true;    // temp. Animation event will change myButtonIsActive in the future
+            Debug.Log("Button is true");
+            //yield return StartCoroutine(myTargetTile);
+            yield return null;
+        }
+        else if (myButtonIsActive == true)
+        {
+            myButtonIsActive = false;   // temp. Animation event will change myButtonIsActive in the future
+            Debug.Log("Button is false");
+            //yield return StartCoroutine(myTargetTile);
+            yield return null;
+        }
     }
 }
