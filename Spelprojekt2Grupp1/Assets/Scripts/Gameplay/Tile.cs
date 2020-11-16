2b5e8b8bdd6c2e95f5e7a722c0d739831e956332
jesper-lundgren <70626985+jesper-lundgren@users.noreply.github.com>
2020-11-16 13:29:54 +0100

    Future-proofed Tile.cs to support a name and overridable Execute
    Added myName property with default constructor value of "Empty" and made TGAExecute into a virtual for subclasses to override.

--- Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tile.cs
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tile.cs
@@ -8,8 +8,17 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     /// <summary>
+    /// The name of the Tile. Base class has "Empty" as name.
+    /// </summary>
+    public string myName { get; protected set; }
+    protected Tile()
+    {
+        myName = "Empty";
+    }
+
+    /// <summary>
     /// Executes a behaviour according its type
     /// </summary>
     /// <param name="aGameActor">The gameobject belonging to the Player</param>
-    public IEnumerator TGAExecute(GameObject aGameActor) { yield return null; }
+    public virtual IEnumerator TGAExecute(GameObject aGameActor) { yield return null; }
 }
