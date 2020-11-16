1938420badaa05f0351f811a3b1919bbdc44ba5f
CornelisW <39124923+CornelisW@users.noreply.github.com>
2020-11-16 16:35:45 +0100

    Created Oven och Freezer Tiles

---
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/FreezerTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezerTile : Tile
+{
+    FreezerTile()
+    {
+        myName = "Freezer";
+    }
+    /// <summary>
+    /// Change the player state to solid
+    /// </summary>
+    /// <param name="aGameActor"></param>
+    /// <returns></returns>
+    public override IEnumerator TGAExecute(GameObject aGameActor)
+    {
+        //aGameActor. ändra state
+        yield return null;
+    }
+}
---
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/OvenTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvenTile : Tile
+{
+    OvenTile()
+    {
+        myName = "Oven";
+    }
+
+    /// <summary>
+    /// Change the player state to gas
+    /// </summary>
+    /// <param name="aGameActor"></param>
+    /// <returns></returns>
+    public override IEnumerator TGAExecute(GameObject aGameActor)
+    {
+        //aGameActor. ändra state
+        yield return null;
+    }
+}
