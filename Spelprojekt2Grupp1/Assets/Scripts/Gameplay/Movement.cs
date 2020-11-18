1a6dd2c4916fa27b8bd827e89bd1b1be99bfcc40
jesper-lundgren <jesper.lundgren@studerande.thegameassembly.se>
2020-11-18 15:40:56 +0100

    Created generic Movement script
    Created a script that tiles & player can use to move around in world

---
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement
+{
+    public IEnumerator MoveInDirection(Transform aTransform, Vector3 aDirection, float aSpeed)
+    {
+        Vector3 position = aTransform.position;
+        Vector3 target = position + aDirection;
+        float divider = Mathf.Abs(Vector3.Distance(position, target));
+
+        float percentage = 0.0f;
+        while (percentage < 1.0f)
+        {
+            aTransform.position = Vector3.Lerp(position, target, percentage);
+            percentage += Time.deltaTime * aSpeed / divider;
+            yield return null;
+        }
+        aTransform.position = target;
+    }
+}
