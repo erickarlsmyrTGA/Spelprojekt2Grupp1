a2b2da131ac35257df59154d9918662d188aa2d3
LEARNET\david.falck <david.falck@studerande.thegameassembly.se>
2020-11-18 17:42:51 +0100

    snowflake and wind scripting WIP stuff

---
+++ Spelprojekt2Grupp1/Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tile tile = TileManager.ourInstance.TGATryGetTileAt(new Vector3(0, 0, 0));
+        if (tile)
+        {
+            StartCoroutine(tile.TGAExecute(gameObject));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
