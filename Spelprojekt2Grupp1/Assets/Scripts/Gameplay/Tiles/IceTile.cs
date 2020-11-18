bcc82cd685e00e984ff215f321758c6499bdb528
Linnea <39124923+CornelisW@users.noreply.github.com>
2020-11-18 16:08:07 +0100

    Created Ice Tile prefab

---
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/IceTile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceTile : Tile
+{
+    IceTile()
+    {
+        myName = "IceTile";
+        myType = TileType.Ice;
+    }
+
+    /// <summary>
+    /// The player glides in the same direction until it reaches a barrier
+    /// </summary>
+    /// <param name="aGameActor"></param>
+    /// <returns></returns>
+    public override IEnumerator TGAExecute(GameObject aGameActor)
+    {
+        //yield return StartCoroutine(aGameActor.GetComponent<Player>().MoveInDirection(direction, myMoveSpeed));
+        yield return null;
+
+    }
+
+}
