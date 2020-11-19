6b7eae1c29c939323f37472118f77d9f7fd50996
erickarlsmyrTGA <erickarlsmyrTGA@users.noreply.github.com>
2020-11-19 22:24:49 +0100

    Added UI script folder and a UI script

---
+++ Spelprojekt2Grupp1/Assets/Scripts/UI/SwipeArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeArea : MonoBehaviour
+{
+    [SerializeField] RectTransform myRect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float area = InputManager.ourInstance.TGAGetSwipeThreashold();
+
+        myRect.anchorMax = new Vector2(1, 1 - area);
+        myRect.offsetMin = new Vector2(0, 0); // new Vector2(left, bottom); 
+        myRect.offsetMax = new Vector2(0, 0); // new Vector2(-right, -top)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
