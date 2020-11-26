a3c6c7ad25c04b43371c743a50b7a251de6f0239
Lukas Persson <lukas.persson@studerande.thegameassembly.se>
2020-11-26 17:27:07 +0100

    Added restart stage

--- Spelprojekt2Grupp1/Assets/Scripts/Managers/GameManager.cs
+++ Spelprojekt2Grupp1/Assets/Scripts/Managers/GameManager.cs
@@ -17,6 +17,7 @@ public class GameManager : MonoBehaviour
 
     public void RestartCurrentStage()
     {
+        myStageManager.ReloadCurrentStage();
     }
 
     public void TransitionToStage(int aStageIndex)
