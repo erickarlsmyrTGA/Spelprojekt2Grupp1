05aa8281c4c7e053a3c1162e8aa85ec0f5072717
Linnea <linnea.almlof@studerande.thegameassembly.se>
2020-11-30 16:15:37 +0100

    script for out of focus

---
+++ Spelprojekt2Grupp1/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+   [SerializeField] GameObject myPauseMenu;
+
+   void OnApplicationPause(bool aPause)
+   {
+      if (aPause == true)
+      {
+         myPauseMenu.SetActive(true);
+         Debug.Log("Pausad!");
+      }
+   }
+
+   private void OnApplicationFocus(bool aFocus)
+   {
+      if (aFocus == false)
+      {
+         myPauseMenu.SetActive(true);
+         Debug.Log("Ur Fokus!");
+      }
+   }
+
+   public void ActivateGame()
+   {
+      myPauseMenu.SetActive(false);
+      Debug.Log("INTE pausad!");
+   }
+}
