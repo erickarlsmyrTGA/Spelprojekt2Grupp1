4672e2453073ac103a205aff411443377987698d
Lukas Persson <lukas.persson@studerande.thegameassembly.se>
2020-12-04 16:06:15 +0100

    Trying rotating snowflake

---
+++ Spelprojekt2Grupp1/Assets/Scripts/Environment/RotatingObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour
+{
+    [SerializeField] public float myRotationSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, myRotationSpeed * Time.deltaTime * 10.0f, 0.0f, Space.Self);
+    }
+}
