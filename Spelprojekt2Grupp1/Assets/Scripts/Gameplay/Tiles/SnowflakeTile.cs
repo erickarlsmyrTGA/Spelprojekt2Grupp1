ac7a9c89ff026dcebd5adcc9eabad9fb272fb8b1
Lukas Persson <lukas.persson@studerande.thegameassembly.se>
2020-12-08 17:16:20 +0100

    Removed unnecessary comments

--- Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/SnowflakeTile.cs
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/SnowflakeTile.cs
@@ -12,52 +12,7 @@ public class SnowflakeTile : Tile
         myName = "Snowflake";
         myType = TileType.Barrier | TileType.Ground;
     }
-    /*
-     * 
-     * Det måste nog finnas en privat player-klass 
-     * att referera till m. typ myPlayer. 
-     * Skapa en private void OnCollisionEnter,
-     * alt en OnTriggerEnter. Beror väl på, lite.
-     * 
-     * i den:
-     * 
-     
-       if (myPlayer == null (inte viktigt nu->)|| myCogSound == null)
-        {
-            return;
-        }
-
-     * 
-     * sen:
-     * 
-        if (aCollision.collider.gameObject.layer == LayerMask.NameToLayer("PlayerLayer"))
-        {
-            myPlayer.IncreaseScore();
-            (inte viktigt nu->)Instantiate(myCogSound, mySoundContainer.transform);
-            Destroy(gameObject);
-        }
-     * 
-     * (LayerMask är en struct som kommer från UnityEngine 
-     * och bär medlemsfunktoinen NameToLayer.
-     * Det var något som Morgan lade till f.a. 
-     * cogs:en till Fenekk hade en attraction, 
-     * och är därmed inget som behövs.)
-     * 
-     * (IncreaseScore(); är en public void-funktion som
-     * kan ligga i player-script, och när den kallas, 
-     * ökar en private int (typ myScore) med 1 varje gång
-     * den öppnas. Det här kanske ska köras med klass, på
-     * samma sätt i Kasino-uppgifterna. Kanske. Idk.)
-     * 
-     * 
-     * 
-     * Vidare tankar om kod finns i Davids anteckningsblock. 
-     * Fråga honom om du läser igenom detta och har tankar.
-     * 
-     * 
-     * 
-     */
-
+   
     static public int myIdGenerator = 0;
     public int myId { get; private set; }
 
