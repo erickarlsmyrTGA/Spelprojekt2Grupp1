4fa7a02dec4cf8ed75633872a9efdd0db0d0f589
PerryIvic <71003066+PerryIvic@users.noreply.github.com>
2020-12-08 18:06:42 +0100

    Added jump effect in ground tile

--- Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/GroundTile.cs
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/GroundTile.cs
@@ -9,4 +9,10 @@ public class GroundTile : Tile
         myName = "Ground";
         myType = TileType.Ground | TileType.Barrier;
     }
+
+    public override IEnumerator TGAExecute()
+    {
+        GameManager.ourInstance.myAudioManager.PlaySFXClip("Thunk_05");
+        return base.TGAExecute();
+    }
 }
