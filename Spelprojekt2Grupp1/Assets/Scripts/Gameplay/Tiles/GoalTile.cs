5d81600bda0ebd80fce7e6779606c853b810fee0
DAVIDmFALCK <71003200+DAVIDmFALCK@users.noreply.github.com>
2020-12-14 17:10:07 +0100

    Updaterade GoalTile-scriptet.
    Nu kan man inte plocka upp moroten som moln.

--- Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/GoalTile.cs
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/GoalTile.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class GoalTile : Tile
 {
+    [SerializeField]
+    private Player myPlayer;
+
     GoalTile()
     {
         myName = "Goal";
         myType = TileType.Ground | TileType.Barrier;
     }
+
+    //private void OnValidate()
+    //{
+    //    bool isSolidState = FindObjectOfType<Player>().IsSolidState;
+    //    if (isSolidState == true)
+    //    {
+    //        GoalTile.TGAExecute();
+    //    }
+    //}
+
+
+    private void OnValidate()
+    {        
+        myPlayer = FindObjectOfType<Player>();        
+    }
+
 
     /// <summary>
     /// Win stage, trigger GameManager's OnStageCompleted
@@ -16,8 +35,11 @@ public class GoalTile : Tile
     /// <returns></returns>
     public override IEnumerator TGAExecute()
     {
+        if (myPlayer.IsSolidState)
+        {
         //GameManager.ourInstance.myAudioManager.PlaySFXClip("Christmas_Sound", 1.0f, 0.7f);
         GameManager.ourInstance.OnStageCleared();
         yield return null;
+        }
     }
 }
