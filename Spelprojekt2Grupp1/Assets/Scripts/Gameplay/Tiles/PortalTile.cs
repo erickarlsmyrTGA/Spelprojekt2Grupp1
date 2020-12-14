d71d2d98527553fd05612a90c7aaa20b782c9bb7
jesper-lundgren <70626985+jesper-lundgren@users.noreply.github.com>
2020-12-14 17:36:54 +0100

    Added portal tile

---
+++ Spelprojekt2Grupp1/Assets/Scripts/Gameplay/Tiles/PortalTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTile : Tile
+{
+    [SerializeField]
+    SceneReference myScene;
+
+    PortalTile()
+    {
+        myType = TileType.Barrier | TileType.Ground;
+    }
+
+    public override IEnumerator TGAExecute()
+    {
+        // TODO: Use GameManager instead
+        UnityEngine.SceneManagement.SceneManager.LoadScene(myScene);
+        yield return null;
+    }
+}
