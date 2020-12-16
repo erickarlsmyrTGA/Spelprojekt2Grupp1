24ebf24b75a9a147099ca7b2b850d69e5701e023
Lukas Persson <lukas.persson@studerande.thegameassembly.se>
2020-12-16 12:13:26 +0100

    - Fixed button sound on clear data - Added slider sound!

---
+++ Spelprojekt2Grupp1/Assets/Scripts/UI/SliderDragEvents.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SliderDragEvents : MonoBehaviour, IPointerUpHandler
+{
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        GameManager.ourInstance.myAudioManager.PlaySFXClip("Pop_Press");
+    }
+}
