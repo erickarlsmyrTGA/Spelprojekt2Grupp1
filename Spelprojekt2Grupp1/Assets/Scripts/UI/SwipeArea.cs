6fc5eb5a5e05d2e2f809e7c473440703978d1bd3
erickarlsmyrTGA <erickarlsmyrTGA@users.noreply.github.com>
2020-12-16 17:02:21 +0100

    fixed gamebreaking ui bug

--- Spelprojekt2Grupp1/Assets/Scripts/UI/SwipeArea.cs
+++ Spelprojekt2Grupp1/Assets/Scripts/UI/SwipeArea.cs
@@ -24,11 +24,11 @@ public class SwipeArea : MonoBehaviour
         mySnowRect.offsetMin = new Vector2(0, 0); // new Vector2(left, bottom); 
         mySnowRect.offsetMax = new Vector2(0, mySnowRect.offsetMax.y); // new Vector2(-right, -top)
 
-        myRightArrow.anchorMax = new Vector2(1 - tenthOfHeight / 2, 0.5f);
-        myRightArrow.anchorMin = new Vector2(1 - tenthOfHeight / 2, 0.5f);
+        //myRightArrow.anchorMax = new Vector2(1 - tenthOfHeight / 2, 0.5f);
+        //myRightArrow.anchorMin = new Vector2(1 - tenthOfHeight / 2, 0.5f);
 
-        myLeftArrow.anchorMax = new Vector2(tenthOfHeight / 2, 0.5f);
-        myLeftArrow.anchorMin = new Vector2(tenthOfHeight / 2, 0.5f);
+        //myLeftArrow.anchorMax = new Vector2(tenthOfHeight / 2, 0.5f);
+        //myLeftArrow.anchorMin = new Vector2(tenthOfHeight / 2, 0.5f);
     }
 
     // Update is called once per frame
