023a523d3f964704d26afaa32f2a673982b9a568
PerryIvic <71003066+PerryIvic@users.noreply.github.com>
2020-12-16 17:15:59 +0100

    Added button sound in gamecleared button + scaled flake counter in levelselect

---
+++ Spelprojekt2Grupp1/Assets/ButtonSound.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    public void PlayButtonSound()
+    {
+        GameManager.ourInstance.myAudioManager.PlaySFXClip("Pop_Return");
+    }
+}
